08de883bcd3ca33e9ce2d7b95763dfdffdb3ac71
admingiant <artemgiant@gmail.com>
2025-11-27 17:50:23 +0200

    Налаштував колонку із транзакціями

--- app/Filament/Resources/Transactions/Tables/TransactionsTable.php
+++ app/Filament/Resources/Transactions/Tables/TransactionsTable.php
@@ -18,16 +18,17 @@ class TransactionsTable
     {
         return $table
             ->columns([
-                TextColumn::make('bankAccount.id')
-                    ->searchable(),
-                TextColumn::make('transaction_id')
-                    ->searchable(),
+
+
                 TextColumn::make('date')
                     ->date()
                     ->sortable(),
                 TextColumn::make('amount')
                     ->numeric()
                     ->sortable(),
+                TextColumn::make('operation_type')
+                    ->badge(),
+
                 TextColumn::make('currency')
                     ->searchable(),
                 TextColumn::make('counter_account')
@@ -36,57 +37,23 @@ class TransactionsTable
                     ->searchable(),
                 TextColumn::make('counter_bank_name')
                     ->searchable(),
-                TextColumn::make('counter_bic')
-                    ->searchable(),
+
                 TextColumn::make('counter_account_name')
                     ->searchable(),
-                TextColumn::make('ks')
-                    ->searchable(),
+
                 TextColumn::make('vs')
                     ->searchable(),
-                TextColumn::make('ss')
-                    ->searchable(),
-                TextColumn::make('user_identification')
-                    ->searchable(),
+
+
                 TextColumn::make('transaction_type')
                     ->searchable(),
                 TextColumn::make('executor')
                     ->searchable(),
-                TextColumn::make('instruction_id')
-                    ->searchable(),
-                TextColumn::make('payer_reference')
-                    ->searchable(),
-                TextColumn::make('operation_type')
-                    ->badge(),
-                TextColumn::make('expense_type')
-                    ->badge(),
-                TextColumn::make('income_type')
-                    ->badge(),
-                TextColumn::make('document_number')
-                    ->searchable(),
-                TextColumn::make('confirmation_number')
-                    ->searchable(),
-                TextColumn::make('donor_id')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('supplier_id')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('campaign_id')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                 ,
-                TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ,
-                TextColumn::make('deleted_at')
-                    ->dateTime()
-                    ->sortable()
-                    ,
+
+
+
+
+
             ])
 
             ->recordActions([
